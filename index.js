72f4751f2ef2ccd62f5840a21c7f1dd4c4a29023
JurekDev <jurek.mueller@outlook.de>
2020-10-10 16:49:08 +0200

    Added Api Callback
    Node js Callback fuction for the Alexa implementation

--- index.js
+++ index.js
@@ -39,7 +39,23 @@ const GetAPICALLBACK = {
         && request.intent.name === 'GetAPIIntent');
   },
   handle(handlerInput) {
-    //TODO Add API Callback
+    var options = {
+      host: url,
+      port: 80,
+      path: '/resources/wetter/',
+      method: 'POST'
+    };
+    
+    http.request(options, function(res) {
+      console.log('STATUS: ' + res.statusCode);
+      console.log('HEADERS: ' + JSON.stringify(res.headers));
+      res.setEncoding('utf8');
+      res.on('data', function (chunk) {
+        console.log('BODY: ' + chunk);
+      });
+    }).end();
+
+    
   },
 };
 
